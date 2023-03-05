e0339ea7f83a2216515714e787a6320790565d70
Junping Yao <junping.yao@seamoney.com>
2023-03-05 22:58:31 +0800

    更新模型选择gpt和text随机选择

--- chatgptlib_src/config.ts
+++ chatgptlib_src/config.ts
@@ -2,7 +2,8 @@
 // gpt-3.5-turbo text-davinci-003 text-curie-001 text-babbage-001 text-ada-001
 // export const CHATGPT_MODEL = "text-davinci-003";
 
-export const CHATGPT_MODEL = process.env.CHATGPT_MODEL || "text-ada-001";
-export const CHATGPT_MODEL_GPT = "gpt-3.5-turbo";
+export const CHATGPT_MODEL = process.env.CHATGPT_MODEL || "text-ada-001"; // 是gpt-3.5-turbo 就用另外一个
+export const CHATGPT_MODEL_GPT =
+  process.env.CHATGPT_MODEL_GPT || "gpt-3.5-turbo";
 export const USER_LABEL_DEFAULT = "User";
 export const ASSISTANT_LABEL_DEFAULT = "ChatGPT";
